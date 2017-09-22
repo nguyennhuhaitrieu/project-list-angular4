9ddadb64a3946be00da036137c9ef4bfd5f0bcba
nguyennhuhaitrieu <nguyennhuhaitrieu@gmail.com>
2017-09-22 15:06:30 +0700

    update

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { ProjectListComponent } from './project-list/project-list.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ProjectListComponent
   ],
   imports: [
     BrowserModule
---
+++ src/app/project-list/project-list.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-project-list',
+  templateUrl: './project-list.component.html',
+})
+export class ProjectListComponent {
+  title = 'app';
+}
