8657b83fd23108ec286568f518bbac26bde9d50b
nguyennhuhaitrieu <nguyennhuhaitrieu@gmail.com>
2017-09-22 16:49:52 +0700

    update

--- src/app/project-list/components/project-list.component.ts
+++ src/app/project-list/components/project-list.component.ts
@@ -10,19 +10,19 @@ export class ProjectListComponent {
 
   constructor() {
     this.courses.push(
-      new Course("C01","Ruby On Rilas","c01.jpg","Lorem ipsum dolor sit amet Lorem ipsum dolor sit   amet", 48 ,["php","angular","front-end"] )
+      new Course("C01","Ruby On Rilas","c01.jpg","Lorem ipsum dolor sit amet Lorem ipsum dolor sit   amet text1", 48 ,["php","angular","front-end"] )
     );
 
     this.courses.push(
-      new Course("C02","IOS Developer","c02.jpg","Lorem ipsum dolor sit amet Lorem ipsum dolor sit   amet", 48 ,["php","angular","jquery"] )
+      new Course("C02","IOS Developer","c02.jpg","Lorem ipsum dolor sit amet Lorem ipsum dolor sit   amet text2", 65 ,["php","angular","jquery"] )
     );
 
     this.courses.push(
-      new Course("C03","PHP Developer","c03.jpg","Lorem ipsum dolor sit amet Lorem ipsum dolor sit   amet", 48 ,["php","angular","nodejs"] )
+      new Course("C03","PHP Developer","c03.jpg","Lorem ipsum dolor sit amet Lorem ipsum dolor sit   amet text3", 80 ,["php","angular","nodejs"] )
     );
 
     this.courses.push(
-      new Course("C04","Java Developer","c04.jpg","Lorem ipsum dolor sit amet Lorem ipsum dolor sit   amet", 48 ,["php","angular","reactjs"] )
+      new Course("C04","Java Developer","c04.jpg","Lorem ipsum dolor sit amet Lorem ipsum dolor sit   amet text4", 55 ,["php","angular","reactjs"] )
     );
   }
 }
