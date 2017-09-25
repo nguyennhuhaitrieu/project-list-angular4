b3ac5ba90914738303b41a3308e6c9e5902755be
nguyennhuhaitrieu <nguyennhuhaitrieu@gmail.com>
2017-09-25 16:53:49 +0700

    update

--- src/app/project-list/pipes/courseSummaryString.pipe.ts
+++ src/app/project-list/pipes/courseSummaryString.pipe.ts
@@ -7,12 +7,13 @@ import { Pipe, PipeTransform } from '@angular/core';
 export class CourseSummaryStringPipe implements PipeTransform {
   transform(value: string, typeSummary: number = 0 ): string {
     if(value) {
-      let limit: number = 10;
-      if(typeSummary == 1  ) limit = 40;
-      if(typeSummary == 2  ) return value;
+      let limit: number = 0;
+      if(typeSummary == 0 ) limit = 10;
+      if(typeSummary == 1 ) limit = 40;
+      if(typeSummary == 2 ) return value;
 
       return value.substring(0, limit) + "...";
-    }else {
+    } else {
       return '';
     }
   }
